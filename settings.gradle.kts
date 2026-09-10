5effcf1856bae4b74869883481dd5553198c5888
SuperMonster003 <30370009+SuperMonster003@users.noreply.github.com>
2026-09-10 10:27:17 +0800

    chore: remove obsolete compatibility and version data

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
         google()
     }
     plugins {
-        id("io.github.supermonster003.autojs6-platform-versions") version "1.6.0"
+        id("io.github.supermonster003.autojs6-platform-versions") version "1.7.4"
         id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     }
 }
